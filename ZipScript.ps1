03eb704c2103d7edd370bcad4596f6b3e8d6e659
ChristiaanSaaiman <csaaiman@retrorabbit.co.za>
2017-02-23 21:35:48 +0200

    No longer need to change the source variable to your userprofile

--- ZipScript.ps1
+++ ZipScript.ps1
@@ -3,10 +3,9 @@
 # Author: Christiaan Saaiman
 # Date: 23/02/2017
 # Requires: ExecutionPolicy be set to RemoteSigned [Set-ExecutionPolicy -ExecutionPolicy RemoteSigned]
-# NB!!! Change user section of source path to match your current computer user
 # --------------------------------------------------------------------------------------
 
-$source = "C:\Users\schut\OneDrive\Hacking-Hares\HackingHares\*.cs"
+$source = $env:USERPROFILE + "\OneDrive\Hacking-Hares\HackingHares\*.cs"
 $destination = "HackingHares.zip"
 
 Compress-Archive -Path $source -DestinationPath $destination
