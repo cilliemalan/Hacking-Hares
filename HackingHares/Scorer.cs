893fcb5c45308f49a5f819e5a8905b89bac2d182
Cillié Malan <cilliemalan@gmail.com>
2017-02-23 20:28:43 +0200

    Added stub scorer

---
+++ HackingHares/Scorer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackingHares
+{
+    public class Scorer
+    {
+        public static int Score(InputStructure input, OutputStructure output)
+        {
+            return 0;
+        }
+    }
+}
