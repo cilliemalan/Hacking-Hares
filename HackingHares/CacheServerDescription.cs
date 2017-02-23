3251223183c71e1a8bdc15f3f48a4e8f84bd1885
Cillié Malan <cilliemalan@gmail.com>
2017-02-23 20:47:56 +0200

    Cache server tostring for debugging

--- HackingHares/CacheServerDescription.cs
+++ HackingHares/CacheServerDescription.cs
@@ -6,6 +6,15 @@ namespace HackingHares
     {
         public int Id { get; set; }
         public List<int> VideoIds { get; set; } = new List<int>();
+
+        /// <summary>
+        /// Not calculated, just for convenience
+        /// </summary>
         public int Usage { get; set; }
+
+        public override string ToString()
+        {
+            return $"Cache #{Id} has {VideoIds.Count} videos with total size of {Usage}MB";
+        }
     }
 }
